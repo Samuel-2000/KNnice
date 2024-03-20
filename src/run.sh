f41765c8ba0c4d92753dbb9a99c30a603fab7ae7
Samuel Kuchta <xkucht11@stud.fit.vutbr.cz>
2024-03-20 01:15:45 +0100

    small path fix

--- src/run.sh
+++ src/run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-workspace=/content/drive/MyDrive
+workspace=/content/KNnice
 program=$workspace/src/__main__.py
 export PYTHONPATH=$workspace/src:$PYTHONPATH
+
 
 python $program --train --adam --NNmodel 2 --dataset 3 --epochs 3
