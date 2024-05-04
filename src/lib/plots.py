d907e78cd761d6f0e8c07b2a556d343765466b07
Samuel Kuchta <xkucht11@stud.fit.vutbr.cz>
2024-05-05 01:03:56 +0200

    update

--- src/lib/plots.py
+++ src/lib/plots.py
@@ -19,33 +19,32 @@ font.set_size(20)
 
 
 def plot_depth_activations(model, test_loader, device, save_path):
-    inputs, targets = next(iter(test_loader))
-    print(len(inputs))
-    print(len(inputs[0]))
-    inputs = inputs.to(device)
-    targets = targets.to(device)
+    image, target = next(iter(test_loader))[0]
+    print(len(image))
+    image = image.to(device)
+    target = target.to(device)
     
     # Forward pass
-    outputs = model(inputs)
+    output = model(image)
     
     # Convert tensors to numpy arrays
-    inputs_np = inputs.cpu().numpy()
-    targets_np = targets.cpu().numpy()
-    outputs_np = outputs.cpu().numpy()
+    image_np = image.cpu().numpy()
+    target_np = target.cpu().numpy()
+    output_np = output.cpu().numpy()
 
     # Plot images
     fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(12, 8))
 
-    # Plot input image
-    axes[0].imshow(inputs_np[0].transpose(1, 2, 0))  # Assuming channels-last format
-    axes[0].set_title('Input Image')
+    # Plot image
+    axes[0].imshow(image_np[0].transpose(1, 2, 0))  # Assuming channels-last format
+    axes[0].set_title('image Image')
 
     # Plot ground truth depth
-    axes[1].imshow(targets_np[0].squeeze(), cmap='gray')
+    axes[1].imshow(target_np[0].squeeze(), cmap='gray')
     axes[1].set_title('Ground Truth Depth')
 
     # Plot network's depth prediction
-    axes[2].imshow(outputs_np[0].squeeze(), cmap='gray')
+    axes[2].imshow(output_np[0].squeeze(), cmap='gray')
     axes[2].set_title('Network Depth Prediction')
 
     # Save the figure
