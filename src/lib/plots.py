45c480d0f272d7f644e4fdea9bfddb23bfb63d83
Samuel Kuchta <xkucht11@stud.fit.vutbr.cz>
2024-05-05 13:40:25 +0200

    .

--- src/lib/plots.py
+++ src/lib/plots.py
@@ -18,41 +18,62 @@ font = FontProperties()
 font.set_size(20)
 
 
-def plot_depth_activations(model, first_test_case, device, save_path):
+
+def plot_depth_activations(model, first_test_case, device, save_path=None):
     image, target = first_test_case
-    print(len(image))
-    print(len(target))
+    print("Image shape:", image.shape)
     
     image = image.to(device)
     
     # Forward pass
-    output = model(image)
-    
+    output = model(image.unsqueeze(0))  # Add batch dimension
+
     # Convert tensors to numpy arrays
     image_np = image.cpu().numpy()
     target_np = target.cpu().numpy()
-    output_np = output.cpu().numpy()
+    output_np = output.detach().cpu().numpy()
 
     # Plot images
-    fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(12, 8))
+    fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(30, 30))
 
     # Plot image
-    axes[0].imshow(image_np[0].transpose(1, 2, 0))  # Assuming channels-last format
-    axes[0].set_title('image Image')
+    if len(image_np.shape) == 2:  # Grayscale image
+        axes[0].imshow(image_np, cmap='gray')
+    else:  # Color image
+        axes[0].imshow(image_np.transpose(1, 2, 0))  # Assuming channels-last format
+    axes[0].set_title('Image')
 
+    # Save the image subplot
+    if save_path:
+        image_save_path = os.path.splitext(save_path)[0] + '_image.png'
+        plt.savefig(image_save_path)
+        plt.close()
+    
     # Plot ground truth depth
-    axes[1].imshow(target_np[0].squeeze(), cmap='gray')
+    axes[1].imshow(target_np.squeeze(), cmap='gray')
     axes[1].set_title('Ground Truth Depth')
 
+    # Save the ground truth depth subplot
+    if save_path:
+        target_save_path = os.path.splitext(save_path)[0] + '_ground_truth.png'
+        plt.savefig(target_save_path)
+        plt.close()
+
     # Plot network's depth prediction
-    axes[2].imshow(output_np[0].squeeze(), cmap='gray')
+    axes[2].imshow(output_np.squeeze(), cmap='gray')
     axes[2].set_title('Network Depth Prediction')
+
+    # Save the network depth prediction subplot
+    if save_path:
+        output_save_path = os.path.splitext(save_path)[0] + '_network_prediction.png'
+        plt.savefig(output_save_path)
+        plt.close()
 
     # Show the figure
     plt.tight_layout()
     plt.show()
 
-    # Save the figure if save_path is provided
+    # Save the original figure if save_path is provided
     if save_path:
         plt.savefig(save_path)
         plt.close()
